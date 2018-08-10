6022bc012d927b3cd74c9052fbf592eed315a50c
Parthasarathi Nataraj <sharewithsarathi@gmail.com>
2018-08-10 10:12:06 -0400

    Add html text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$('.base').clone().addClass('overlay').appendTo('.landing');
+$('.cta').hover(function() {
+  $('.overlay').toggleClass('over');
+});
+
+/*--------------------
+Codepen Preview Tile
+--------------------*/
+setTimeout(function() {
+  $('.overlay').addClass('over').delay(600).queue(function() {
+    $(this).removeClass("over").dequeue();
+  });
+}, 400)
